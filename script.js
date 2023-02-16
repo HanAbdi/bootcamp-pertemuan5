42efc8bcd878e2109e8f535af22be6b79fdf3c95
HanAbdi <muhammadroihan3@gmail.com>
2023-02-16 11:44:46 +0700

    Pertemuan 5

---
+++ script.js
@@ -0,0 +1,134 @@
+// var a = 10
+// let b = 20
+// // const untuk satu blok
+// const c = a + b
+// console.log(a)
+// console.log(b)
+// console.log(c)
+
+// var int = 5;
+// let float = 5.5;
+
+// var string1 = "Aku";
+// var string2 = "Pinter";
+// console.log(string1 + string2)
+
+// penjumlahan dalam java script
+// var a = 10
+// var b = 20
+// var hasil = a + b;
+// console.log(a + hasil)
+
+// perkalian dalam js
+// var a = 6;
+// var t = 8;
+// var luas = 0.5 * a * t;
+// console.log(luas)
+
+// var modulus1 = 15
+// var modulus2 = 10
+// var hasil = modulus1 % modulus2
+// console.log(hasil)
+
+// var array = [1, 2, 3, 4, 5];
+// console.log(array[2])
+
+// var object = {nama: "Roihan", nim: "422210001", jurusan: "TI"}
+// console.log(object.nama)
+
+// control flow dalam pemograman adalah perintah kondisional
+// var angka = prompt('Masukkan angka : ');
+// kondisi or
+// if (angka == "10" || angka == "20") {
+//     alert('user memasukkan angka ' + angka)
+// } else {
+//     alert('angka yang dimasukkan salah')
+// }
+
+//kondisi dan
+// if (angka == "10" && angka == "20") {
+//     alert('user memasukkan angka ' + angka)
+// } else {
+//     alert('angka yang dimasukkan salah')
+// }
+
+// menentukan ganjil genap
+// if (angka % 2 == 0) {
+//     alert(angka + " adalah bilangan genap")
+// } else {
+//     alert(angka + " adalah bilangan ganjil")
+// }
+
+// var test = 
+
+// var sistem = 10
+// var sistem_run = 6
+// var no_sistem = 1
+// while menggunakan if else
+// while (no_sistem <= sistem_run) {
+//     console.log("sistem no. " + no_sistem + " Running")
+//     if (no_sistem >= 5) {
+//         console.log("sistem no. " + no_sistem + " Maintenance")
+//     } else {
+//         console.log("sistem no. " + no_sistem + " Running")
+//     }
+//     no_sistem++
+// }
+
+// for (no_sistem = sistem_run + 1) {
+//     
+    
+// }
+
+// for (no_sistem = 1 ; no_sistem <= sistem ; no_sistem++) {
+//     if (no_sistem >= 8) {
+//         console.log("sistem no. " + no_sistem + " Maintenance")
+//     } else {
+//         console.log("sistem no. " + no_sistem + " Running")
+//     }
+// }
+
+// var number = prompt('Silahkan masukkan angka : ')
+// switch (number) {
+//     case "1":
+//         alert('Anda memasukkan angka 1');
+//         break;
+//     case "2":
+//         alert('Anda memasukkan angka 2');
+//         break;
+//     case "3":
+//         alert('Anda memasukkan angka 3');
+//     case "4":
+//         alert('Anda memasukkan angka 4');
+//         break;
+//     default:
+//         alert('Anda memasukkan angka yang TIDAK TERSEDIA');
+//         break;
+// }
+
+//function adalan blok kode terorganisir dapat digunakan kembali atau digunakan untuk melakukan sebuah tindakan atau function
+// function test(a, b) {
+//     var total;
+    
+//     return total = a + b
+// }
+
+// console.log(test(10, 20));
+
+// var array = [];
+// array[0] = "index1"
+// array[1] = "index2"
+// array[2] = "index3"
+// array[3] = "index4"
+// console.log(array);
+
+var array = ["Roihan", "422210001", "Teknik Informatika"]
+// for (let i=0; i<3; i++) {
+//     const element = array[index];
+    
+// }
+array.forEach(function(e, 1))
+(
+    function(e, 1)
+    {console.log(e, 1)}
+)
